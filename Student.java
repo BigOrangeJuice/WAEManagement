cb8c782ebed0295309632be1502ea81320d2af0a
ythree33 <82850377+ythree33@users.noreply.github.com>
2021-08-12 19:17:29 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -28,13 +28,16 @@ public class Student extends Person {
     }
 
     //添加学生信息
-    public void plus(Scanner scanner) {
+    public void plus(List<Person> students,Scanner scanner) {
         System.out.println("请输入你想要输入的学生信息：");
 
         System.out.println("学号：         请输入12位有效数字");
         while(this.getId() == 0) {
             long id = scanner.nextLong() ;
-            if (ToolClass.digits(new Student(),id)) this.setId(id);
+            if (ToolClass.digits(new Student(),id)){
+                if(!ToolClass.same(students,id))      this.setId(id);
+                else System.out.println("该学号已存在，请重新输入！");
+            }
             else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
         }
         System.out.println("姓名：         请按中国人的名字格式输入");
@@ -59,18 +62,11 @@ public class Student extends Person {
         //有多个消费品的用量和缴费情况，创建消费品集合
         List<Consumption> consumptions = new ArrayList<>();
 
-        Consumption consumptionE = new Consumption() ;
-        consumptions.add(consumptionE) ;
-
-        Consumption consumptionW = new Consumption() ;
-        consumptions.add(consumptionW) ;
-
-        System.out.println(consumptions.size());
-
         //用电情况
         Consumption electric = new Consumption();
         System.out.println("用电量：");
-        while(this.getConsumptions().get(0).getConsumption() == 0.0){
+
+        while(electric.getConsumption() == 0.0){
             double consumption = scanner.nextDouble() ;
             if(ToolClass.styleConsumption(consumption))    electric.setConsumption(consumption);
             else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
@@ -85,12 +81,12 @@ public class Student extends Person {
             }
             else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
         }
-        consumptionE = electric;
+        consumptions.add(electric);
 
         //用水情况
         Consumption water = new Consumption();
         System.out.println("用水量：");
-        while(this.getConsumptions().get(1).getConsumption() == 0.0){
+        while(water.getConsumption() == 0.0){
             double consumption = scanner.nextDouble() ;
             if(ToolClass.styleConsumption(consumption))    water.setConsumption(consumption);
             else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
@@ -105,7 +101,7 @@ public class Student extends Person {
             }
             else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
         }
-        consumptionW = water;
+        consumptions.add(water) ;
 
         this.setConsumptions(consumptions);
     }
@@ -125,10 +121,12 @@ public class Student extends Person {
                 for (int j = 0; j < students.get(i).getConsumptions().size(); ++j) {
                     if (j == 0) {
                         System.out.println("用电量：" + students.get(i).getConsumptions().get(j).getConsumption() + " 度");
+                        System.out.printf("应交电费：%.2f 元\n",students.get(i).getConsumptions().get(j).getConsumption()*0.588);
                         System.out.println("是否缴电费：" + (students.get(i).getConsumptions().get(j).isPay() == true ? "已缴" : "未缴"));
                     }
                     if (j == 1) {
                         System.out.println("用水量：" + students.get(i).getConsumptions().get(j).getConsumption() + " 千升");
+                        System.out.printf("应交水费：%.2f 元\n",students.get(i).getConsumptions().get(j).getConsumption()*3.10);
                         System.out.println("是否缴水费：" + (students.get(i).getConsumptions().get(j).isPay() == true ? "已缴" : "未缴"));
                     }
                 }
@@ -154,10 +152,12 @@ public class Student extends Person {
             for (int j = 0; j < students.get(i).getConsumptions().size(); ++j) {
                 if (j == 0) {
                     System.out.println("用电量：" + students.get(i).getConsumptions().get(j).getConsumption() + " 度");
+                    System.out.printf("应交电费：%.2f 元\n",students.get(i).getConsumptions().get(j).getConsumption()*0.588);
                     System.out.println("是否缴电费：" + (students.get(i).getConsumptions().get(j).isPay() == true ? "已缴" : "未缴"));
                 }
                 if (j == 1) {
                     System.out.println("用水量：" + students.get(i).getConsumptions().get(j).getConsumption() + " 千升");
+                    System.out.printf("应交水费：%.2f 元\n",students.get(i).getConsumptions().get(j).getConsumption()*3.10);
                     System.out.println("是否缴水费：" + (students.get(i).getConsumptions().get(j).isPay() == true ? "已缴" : "未缴"));
                 }
             }
@@ -170,21 +170,51 @@ public class Student extends Person {
             if (name.equals(students.get(i).getName())) {
                 System.out.println("请确认是否要编辑" + name + "的信息：");
                 System.out.println("1 - 当然，我确定 | 2 - 我再考虑考虑");
+
                 int target = scanner.nextInt() ;
                 switch (target){
                     case 1:
+                        //将元素初始化
+                        students.get(i).setId(0);
+                        students.get(i).setName(null);
+                        students.get(i).setSex('\u0000');
+                        students.get(i).setConsumptions(null);
+
                         System.out.println("请输入新的学生信息：");
 
                         System.out.println("学号：         请输入12位有效数字");
-                        students.get(i).setId(scanner.nextLong());
+
+                        while(students.get(i).getId() == 0) {
+                            long id = scanner.nextLong() ;
+                            if (ToolClass.digits(new Student(),id)){
+                                if(!ToolClass.same(students,id))      students.get(i).setId(id);
+                                else System.out.println("该学号已存在，请重新输入！");
+                            }
+                            else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
+                        }
+
                         System.out.println("姓名：         请按中国人的名字格式输入");
-                        students.get(i).setName(scanner.next());
+                        while (students.get(i).getName() == null) {
+                            String name1 = scanner.next() ;
+                            if(ToolClass.styleName(name1))    students.get(i).setName(name1);
+                            else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
+                        }
+
                         System.out.println("性别：         男/女");
-                        students.get(i).setSex(scanner.next().charAt(0));
+                        while(students.get(i).getSex() == '\u0000') {
+                            char sex = scanner.next().charAt(0) ;
+                            if(ToolClass.styleSex(sex)) students.get(i).setSex(sex);
+                            else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
+                        }
+
                         System.out.println("专业班级：      格式：专业-班级");
                         if (students.get(i) instanceof Student) {
                             Student student = (Student) students.get(i);
-                            student.proClass = scanner.next() ;
+                            while (student.proClass == null) {
+                                String proClass = scanner.next();
+                                if (ToolClass.styleProClass(proClass)) student.setProClass(proClass);
+                                else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
+                            }
                         }
 
                         //有多个消费品的用量和缴费情况，创建消费品集合
@@ -193,17 +223,41 @@ public class Student extends Person {
                         //用电情况
                         Consumption electric = new Consumption();
                         System.out.println("用电量：");
-                        electric.setConsumption(scanner.nextDouble());
+                        while(electric.getConsumption() == 0.0){
+                            double consumption = scanner.nextDouble() ;
+                            if(ToolClass.styleConsumption(consumption))    electric.setConsumption(consumption);
+                            else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
+                        }
                         System.out.println("是否缴费：   true - 已缴 | false - 未缴");
-                        electric.setPay(scanner.nextBoolean());
+                        String str1 = null ;
+                        while(str1 == null) {
+                            String result = scanner.next() ;
+                            if(result.equals("true") || result.equals("false")){
+                                str1 = result ;
+                                electric.setPay(Boolean.getBoolean(result)) ;
+                            }
+                            else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
+                        }
                         consumptions.add(electric);
 
                         //用水情况
                         Consumption water = new Consumption();
                         System.out.println("用水量：");
-                        water.setConsumption(scanner.nextDouble());
+                        while(water.getConsumption() == 0.0){
+                            double consumption = scanner.nextDouble() ;
+                            if(ToolClass.styleConsumption(consumption))    water.setConsumption(consumption);
+                            else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
+                        }
                         System.out.println("是否缴费：   true - 已缴 | false - 未缴");
-                        water.setPay(scanner.nextBoolean());
+                        String str2 = null ;
+                        while(str2 == null) {
+                            String result = scanner.next() ;
+                            if(result.equals("true") || result.equals("false")){
+                                str2 = result ;
+                                water.setPay(Boolean.getBoolean(result)) ;
+                            }
+                            else System.out.println("---抱歉，你的输入格式有误，请重新输入---");
+                        }
                         consumptions.add(water);
 
                         students.get(i).setConsumptions(consumptions);
@@ -213,6 +267,56 @@ public class Student extends Person {
                         break;
                 }
             }
+        }
+    }
+
+    //删除学生信息
+    public static void delete(List<Person> students, String name,Scanner scanner) {
+        for (int i = 0; i < students.size(); ++i) {
+            if (name.equals(students.get(i).getName())) {
+                System.out.println("请确认是否要删除" + name + "的信息：(删除后不可恢复，请慎重使用！)");
+                System.out.println("1 - 没错，我要删除 | 2 - 我再考虑考虑");
+
+                int target = scanner.nextInt() ;
+                switch (target) {
+                    case 1:
+                        students.remove(i) ;
+                        System.out.println("删除成功！");
+                    case 2:
+                        break;
+                }
+            }
+        }
+    }
+
+    //统计学生使用水电情况
+    public static void statistics(List<Person> students){
+        if (students.size() == 0) {
+            System.out.println("-- 暂无学生信息，请先手动添加 --");
+        }
+
+        List<Person> students1 = students ;
+
+        //从大到小排序
+        for(int i = 0;i < students1.size() - 1;++ i){
+            for(int j = 0;j < students1.size() - 1 - i;++ j){
+                if(students1.get(j).getConsumptions().get(0).getConsumption() < students1.get(j+1).getConsumptions().get(0).getConsumption()){
+                    Person temp = students1.get(j) ;
+                    students1.set(j+1,students1.get(j)) ;
+                    students1.set(j,temp) ;
+                }
+            }
+        }
+        System.out.println("序列\t" + "姓名\t" + "用电量/度\t" + "用水量/m^3\t" + "电费/元\t" + "水费/元\t" + "是否缴电费\t" + "是否缴水费\t" );
+        for(int i = 0;i < students1.size();++ i){
+            System.out.print(i+1 + "\t\t");
+            System.out.print(students1.get(i).getName() + "\t");
+            System.out.printf("%.2f\t\t",students1.get(i).getConsumptions().get(0).getConsumption());
+            System.out.printf("%.2f\t\t",students1.get(i).getConsumptions().get(1).getConsumption());
+            System.out.printf("%.2f\t",students1.get(i).getConsumptions().get(0).getConsumption()*0.588);
+            System.out.printf("%.2f\t",students1.get(i).getConsumptions().get(1).getConsumption()*3.10);
+            System.out.print(students1.get(i).getConsumptions().get(0).isPay() + "\t\t");
+            System.out.println(students1.get(i).getConsumptions().get(1).isPay());
         }
     }
 
