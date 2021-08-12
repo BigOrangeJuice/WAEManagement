92ff45f78363a13d18273cbd34f8fb5064f37689
ythree33 <82850377+ythree33@users.noreply.github.com>
2021-08-12 19:16:53 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main(String[] args) {
 
         //提前创建好学生集合和教工集合
-        List<Person> students = new ArrayList<>(MAX) ;
-        List<Teacher> teachers = new ArrayList<>(MAX) ;
+        List<Person> students = new ArrayList<>(MAX);
+        List<Teacher> teachers = new ArrayList<>(MAX);
 
         while (true) {
             //主选择界面
@@ -28,34 +28,39 @@ public class Main {
                     S_select = sc.nextInt();
                     switch (S_select) {
                         case 1://添加功能
-                            Student student = new Student() ;
-                            student.plus(sc);
-                            students.add(student) ;
+                            Student student = new Student();
+                            student.plus(students, sc);
+                            students.add(student);
                             break;
                         case 2://查询功能
                             System.out.println("请输入你想要查找的学生姓名：");
-                            String nameQuery = sc.next() ;
-                            if(Person.judgeExist(students,nameQuery)){
+                            String nameQuery = sc.next();
+                            if (Person.judgeExist(students, nameQuery)) {
                                 System.out.println("找到此人！");
-                                Student.query(students,nameQuery) ;
-                            }
-                            else System.out.println("抱歉，此人不存在！");
+                                Student.query(students, nameQuery);
+                            } else System.out.println("抱歉，此人不存在！");
                             break;
                         case 3://显示功能
                             Student.show(students);
                             break;
                         case 4://编辑功能
                             System.out.println("请输入你想要编辑的学生姓名：");
-                            String nameModify = sc.next() ;
-                            if(Person.judgeExist(students,nameModify)){
+                            String nameModify = sc.next();
+                            if (Person.judgeExist(students, nameModify)) {
                                 System.out.println("找到此人！");
-                                Student.modify(students,nameModify,sc) ;
-                            }
-                            else System.out.println("抱歉，此人不存在！");
+                                Student.modify(students, nameModify, sc);
+                            } else System.out.println("抱歉，此人不存在！");
                             break;
                         case 5://删除功能
+                            System.out.println("请输入你想要删除的学生姓名：");
+                            String nameDelete = sc.next();
+                            if (Person.judgeExist(students, nameDelete)) {
+                                System.out.println("找到此人！");
+                                Student.delete(students, nameDelete, sc);
+                            } else System.out.println("抱歉，此人不存在！");
                             break;
                         case 6://统计功能
+                            Student.statistics(students);
                             break;
                         case 7://保存功能
                             break;
@@ -63,6 +68,9 @@ public class Main {
                             break;
                         case 0://返回上一级
                             continue;
+                        default:
+                            System.out.println("尊敬的用户，您的输入有误，请按照规定重新输入！");
+                            break;
                     }
                     break;
                 case 2:
@@ -102,6 +110,12 @@ public class Main {
 
     public static void showMenu() {
         System.out.println("\n\n    Welcome to WAEManagement\n\n");
+        System.out.println("***********************************");
+        System.out.println("        近日水电费情况：");
+        System.out.println("        水费：3.10 元/m^3");
+        System.out.println("        电费：0.588 元/度");
+        System.out.println("        节水节电从我做起！！！");
+        System.out.println("***********************************");
         System.out.println("***********************************");
         System.out.println("        1 - 学生");
         System.out.println("        2 - 教工");
