4ee05004157ed46abdcb3429e80a113b187e95d8
ythree33 <82850377+ythree33@users.noreply.github.com>
2021-08-12 19:14:21 +0800

    Create ToolClass.java

---
+++ ToolClass.java
@@ -0,0 +1,50 @@
+package Homework_WAEManagement;
+
+public class ToolClass {
+    //判断位数
+    public static boolean digits(Person person,long id) {
+        if (person instanceof Student) {
+            byte number = (byte) (id / 100000000000l);
+            if (number > 0 && number < 10) return true;
+        }
+        if (person instanceof Teacher) {
+            byte number = (byte) (id / 100000l);
+            if (number > 0 && number < 10) return true;
+        }
+        return false ;
+    }
+
+    //判断名字格式
+    public static boolean styleName(String name){
+        char[] array = name.toCharArray() ;
+        for(int i = 0;i < array.length;++ i){
+            if(!isChinese(array[i]))    return false ;
+        }
+        return true ;
+    }
+
+    //判断性别格式
+    public static boolean styleSex(char sex){
+        if (isChinese(sex)){
+            if(sex == '男' || sex == '女')    return true ;
+        }
+        return false ;
+    }
+
+    //判断是否为汉字
+    public static boolean isChinese(char ch){
+        return String.valueOf(ch).matches("[\u4e00-\u9fa5]") ;
+    }
+
+    //判断专业班级格式
+    public static boolean styleProClass(String proClass){
+        if(proClass.indexOf('-') != -1)     return true ;
+        return false ;
+    }
+
+    //判断用水量和用电量的格式
+    public static boolean styleConsumption(double consumption){
+        if(consumption < 0)     return false ;
+        return  true ;
+    }
+}
