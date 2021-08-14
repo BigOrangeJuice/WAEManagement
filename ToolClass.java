6f54560d289dbf50d7ae486ec22dd59241c7109a
ythree33 <82850377+ythree33@users.noreply.github.com>
2021-08-14 15:28:49 +0800

    Update ToolClass.java

--- ToolClass.java
+++ ToolClass.java
@@ -1,24 +1,35 @@
 package Homework_WAEManagement;
 
+import java.util.List;
+
+//工具类
 public class ToolClass {
     //判断位数
     public static boolean digits(Person person,long id) {
         if (person instanceof Student) {
-            byte number = (byte) (id / 100000000000l);
+            byte number = (byte) (id / 100000000000L);
             if (number > 0 && number < 10) return true;
         }
         if (person instanceof Teacher) {
-            byte number = (byte) (id / 100000l);
-            if (number > 0 && number < 10) return true;
+            byte number = (byte) (id / 100000L);
+            return number > 0 && number < 10;
         }
         return false ;
+    }
+
+    //学号/教工号查重
+    public static boolean same(List<Person> people, long id) {
+        for (Person person : people) {
+            if (id == person.getId()) return false;
+        }
+        return true;
     }
 
     //判断名字格式
     public static boolean styleName(String name){
         char[] array = name.toCharArray() ;
-        for(int i = 0;i < array.length;++ i){
-            if(!isChinese(array[i]))    return false ;
+        for (char c : array) {
+            if (!isChinese(c)) return false;
         }
         return true ;
     }
@@ -26,7 +37,7 @@ public class ToolClass {
     //判断性别格式
     public static boolean styleSex(char sex){
         if (isChinese(sex)){
-            if(sex == '男' || sex == '女')    return true ;
+            return sex == '男' || sex == '女';
         }
         return false ;
     }
@@ -36,15 +47,13 @@ public class ToolClass {
         return String.valueOf(ch).matches("[\u4e00-\u9fa5]") ;
     }
 
-    //判断专业班级格式
+    //判断专业班级
     public static boolean styleProClass(String proClass){
-        if(proClass.indexOf('-') != -1)     return true ;
-        return false ;
+        return proClass.indexOf('-') != -1;
     }
 
     //判断用水量和用电量的格式
     public static boolean styleConsumption(double consumption){
-        if(consumption < 0)     return false ;
-        return  true ;
+        return !(consumption < 0);
     }
 }
